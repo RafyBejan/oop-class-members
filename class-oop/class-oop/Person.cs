24370abf16edcbabfc5b594f91a05ca2b4cdafd8
bejan <bejan@RAFAEL12>
2023-11-10 09:26:11 +0200

    oop-class-members

---
+++ class-oop/class-oop/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_oop
+{
+    internal class Person
+    {
+        public string FirstName = "John";
+        public string LastName = "Doe";
+        public readonly string CNP="1234"; // valoare implicita se poate modificain zona constructor(#1)
+    
+
+        public static int PopulationCount = 0;
+        public static readonly int MaxLifespanYears = InitMaxLifespan();
+     //   public const int Factor = 2; //const in alt const
+     //   public const int  MaxLifespanYears= 60 * Factor + 10; //const sintaxa initializatoare
+         public Person(string cnp) // acesta este un constructor 
+        {     //readonly poate fi modificat in constructor(#2)
+            CNP = "656565";
+            CNP = cnp;
+        }
+        public void PrintPerson()
+        {
+            
+        }
+
+        public static void PrintPopulationCount()
+        {
+
+        }
+        public static int InitMaxLifespan()
+        {
+            Console.Write("Max Lifespan");
+            return Convert.ToInt32(Console.ReadLine());
+        }
+                                                    //static nu tine de un obiect anume,se modifica lucreaza cu classa
+                                                   // instanta nu se modifica se foloseste numele obiectului
+    }
+}
--- class-oop/class-oop/Program.cs
+++ class-oop/class-oop/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int maxlifespan = Person.MaxLifespanYears;
+            Person p = new Person("2344");
+            Console.WriteLine(p.FirstName);
+            Console.WriteLine(p.LastName);
+            Console.WriteLine(p.CNP);
+            Person.PopulationCount = 1; 
+            // p.CNP="2344";
+                                              // static se pune Person nu p.
+            
         }
     }
 }
